142f5f7ccfe1e9a805b4195e052c2637472a0f8a
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-08-20 21:08:53 +0300

    To date

--- js/main.js
+++ js/main.js
@@ -1,75 +1,86 @@
-var courseOne = "FrontEnd course and UI/UX design course",
-    courseTwo = "Backend course"
-
-console.log(courseOne.length)
-console.log(courseOne[0])
-console.log(courseOne.charAt(0))
-console.log(courseOne.charCodeAt(0))
-console.log(String.fromCharCode(70))
-
-console.log(courseOne + courseTwo)
-console.log(`${courseOne} ${courseTwo}`)
-console.log(courseOne.concat(courseTwo))
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 
-console.log(courseOne.startsWith('Front'))
-console.log(courseOne.endsWith('e '))
-console.log(courseOne.includes('end'))
-console.log(courseOne.indexOf('en'))
-console.log(courseOne.indexOf('e'))
-console.log(courseOne.lastIndexOf('e'))
+console.log(fruits.length)
+console.log(fruits.concat([9, 'test']))
+console.log(fruits)
+// console.log(fruits.copyWithin(2, 0))
+// console.log(fruits)
+
+const ages = [3, 5, 100, 18, 20];
+
+console.log(ages.some(function (age) { return age > 18 }))
+console.log(ages.some(checkAge))
+console.log(ages.some(age => age > 18))
+
+function checkAge(age) { return age > 18 }
+
+console.log("*".repeat(15))
+
+console.log(ages.every(function (age) { return age > 18 }))
+console.log(ages.every(checkAge))
+console.log(ages.every(age => age > 18))
 
 
-console.log(courseOne.match(/e/g))
-console.log(courseOne.match(/e/g).length)
-console.log(courseOne.match(/e/gi))
+// console.log(fruits.fill('course', undefined, 3))
+console.log(ages.find(age => age > 18))
+console.log(ages.findIndex(age => age > 188))
+console.log(fruits)
 
 
-console.log(courseTwo.repeat(20))
+console.log(ages.filter(age => age > 18))
+var test = ages.map(age => {
+    if (age > 18) { return ("Your age is over : " + age) }
+    else return ("Your age is under: " + age)
+})
+
+console.log(test)
+var testOne = ages.forEach(age => ("Your age is : " + age))
+console.log(testOne) //undefined
 
 
+console.log(ages)
+console.log(fruits.slice(1, 3))
+// fruits.pop()
+// fruits.push('New fruit')
 
-console.log(courseOne.replace('UI', "test"))
+// fruits.shift()
+// fruits.unshift("Another")
+fruits.splice(1, 0, "New", "new2")
 
-
-console.log(courseOne.search('course'))
-
-
-console.log(courseOne.slice(3, 10))
-
-
-console.log(courseOne.split(' '))
-console.log(["test", "Hello", "OK"].join(' '))
+console.log(fruits)
 
 
-
-var username = 'Mahmoud'
-console.log(username.substring(2, 6))
-console.log(username.substr(2, 6))
+console.log(fruits.join('-'))
 
 
-console.log(username.toUpperCase())
-console.log(username.toLowerCase())
+console.log(fruits.toString())
+console.log(fruits.sort())
 
 
-var message = "    I     am    a    developer      ";
+var numbers = [91, 103, 122, 12, 88]
 
 
+console.log(numbers.sort((a, b) => b - a))
 
-console.log(message)
-console.log(message.trimStart())
-console.log(message.trimEnd())
-console.log(message.trim())
+let i = 0
+
+function sayHello() {
+    console.log("Hello : " + i)
+    i++
+}
 
 
-console.log(Math.random())
-console.log(Math.ceil(5.1))
-console.log(Math.floor(5.999))
-console.log(Math.round(3.4))
-console.log(Math.max(45, 54, 87, 48, 21, 48))
-console.log(Math.min(45, 54, 87, 48, 21, 48))
-console.log(Math.pow(2, 3))
-console.log(Math.sqrt(25))
-console.log(Math.PI)
-console.log(Math.abs(-12))
-console.log(Math.abs(12))
+// var myInterval = setInterval(sayHello, 500)
+var myTimeout = setTimeout(sayHello, 4000)
+
+function stopInterval() {
+    // clearInterval(myInterval)
+    clearTimeout(myTimeout)
+}
+
+var myDate = new Date();
+
+console.log(myDate.getDay())
+myDate.setDate(15)
+console.log(myDate)
---
+++ js/main.ts
@@ -0,0 +1,1 @@
+var x: string | number = 5;
