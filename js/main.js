85e609b2325e89aa18af87f1089f62659796099f
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-09-03 21:29:08 +0300

    End Boostrap components

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
-//  input >> JavaScript is my favorite language
-/*
-output >>
-1- JavaScript
-2- is
-3- my
-4- favorite
-5- language
-*/
+//You get an array of numbers, return the sum of all of the positives ones.
+//Note: if there is nothing to sum, the sum is default to 0.
 
-function ordering(str) {
-    let arr = str.split(' ')
 
-    for (let index = 0; index < arr.length; index++) {
-        const element = arr[index];
-        console.log(index + 1 + '- ' + element)
+function sumOfArray(arr) {
+    let sum = 0;
+
+    if (Array.isArray(arr)) {
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+            if (element > 0) {
+                sum += element
+            }
+        }
     }
+
+    return sum;
+
 }
 
-ordering('JavaScript is my favorite language')
 
+console.log(sumOfArray([5, 7, 8, 9, -45, 10]))
+console.log(sumOfArray([]))
+
+
+console.log("-".repeat(20))
 /************************************************** Task **************************************************/
 // Present odd numbers from 0 to 20 only
+
+var sum = 0;
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 1) {
+        sum += i;
+    }
+}
+console.log(sum)
